2ec657941e72302bccf8019c62eb6441370af543
Jordi-Cordoba <jordimanuelcordoba@gmail.com>
2022-08-22 13:04:03 +0200

    Condiciones y Operadores

---
+++ main.js
@@ -0,0 +1,12 @@
+function CaraDeLaMoneda() {
+    let CaraOCruz = Math.round(Math.random())
+    if (CaraOCruz == 0) {
+        console.log('Cara');
+    }
+    else if (CaraOCruz == 1) {
+        console.log('Cruz');
+    }
+}
+CaraDeLaMoneda()
+
+
